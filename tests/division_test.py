6437aaa27a9758c810cbd7130e87e803c5643a2f
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-11-11 12:07:11 -0500

    merge fix attempt

---
+++ tests/division_test.py
@@ -0,0 +1,11 @@
+"""Testing Addition"""
+from calc.calculations.division import Division
+
+def test_calculation_division():
+    """testing that our calculator has a static method for addition"""
+    #Arrange
+    mynumbers = (4.0,2.0)
+    division = Division(mynumbers)
+    #Act
+    #Assert
+    assert division.get_result() == 2.0
