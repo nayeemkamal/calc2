96a49d965f997d6547190cd6008513e99cb89e04
Nayeem Kamal <kamal.nayeem12@gmail.com>
2021-11-11 08:05:01 -0500

    working calc

--- calc/history/calculations.py
+++ calc/history/calculations.py
@@ -1,40 +1,42 @@
 """Calculation history Class"""
-from calc.calculations.addition import Addition
-from calc.calculations.subtraction import Subtraction
-from calc.calculations.multiplication import Multiplication
-class Calculations:
-    history = []
+from history import History
+class history.
+    history = History()
     # pylint: disable=too-few-public-methods
-    @staticmethod
-    def clear_history():
-        Calculations.history.clear()
-        return True
-    @staticmethod
-    def count_history():
-        return len(Calculations.history)
-    @staticmethod
-    def get_last_calculation():
-        return Calculations.history[-1]
-    @staticmethod
-    def get_first_calculation():
-        return Calculations.history[-1]
-    @staticmethod
-    def get_calculation(num):
-        """ get a specific calculation from history"""
-        return Calculations.history[num]
+    # @staticmethod
+    # def clear_history():
+    #     history.clear()
+    #     return True
+    # @staticmethod
+    # def count_history():
+    #     return len(history)
+    # @staticmethod
+    # def get_last_calculation():
+    #     return history[-1]
+    # @staticmethod
+    # def get_first_calculation():
+    #     return history[-1]
+    # @staticmethod
+    # def get_calculation(num):
+    #     """ get a specific calculation from history"""
+    #     return history[num]
     @staticmethod
     def add_calculation(calculation):
         """ get a specific calculation from history"""
-        return Calculations.history.append(calculation)
+        return history.add_calculation(calculation)
     @staticmethod
     def add_addition_calculation(values):
-        Calculations.add_calculation(Addition(values))
-        return Calculations.get_last_calculation().get_result()
+        history.add_addition_calculation(values)
+        return history.get_last_calculation().get_result()
     @staticmethod
     def add_subtraction_calculation(values):
-        Calculations.add_calculation(Subtraction(values))
-        return Calculations.get_last_calculation().get_result()
+        history.add_subtraction_calculation(values)
+        return history.get_last_calculation().get_result()
     @staticmethod
     def add_multiplication_calculation(values):
-        Calculations.add_calculation(Multiplication(values))
-        return Calculations.get_last_calculation().get_result()
+        history.add_multiplication_calculation(values)
+        return history.get_last_calculation().get_result()
+    @staticmethod
+    def add_division_calculation(values):
+        History.add_division_calculation(values)
+        return History.get_last_calculation().get_result()
---
+++ calc/history/history.py
@@ -0,0 +1,46 @@
+"""Calculation history Class"""
+from calc.calculations.addition import Addition
+from calc.calculations.subtraction import Subtraction
+from calc.calculations.multiplication import Multiplication
+from calc.calculations.division import Division
+
+class History:
+    history = []
+    # pylint: disable=too-few-public-methods
+    @staticmethod
+    def clear_history():
+        History.history.clear()
+        return True
+    @staticmethod
+    def count_history():
+        return len(History.history)
+    @staticmethod
+    def get_last_calculation():
+        return History.history[-1]
+    @staticmethod
+    def get_first_calculation():
+        return History.history[-1]
+    @staticmethod
+    def get_calculation(num):
+        """ get a specific calculation from history"""
+        return History.history[num]
+    @staticmethod
+    def add_calculation(calculation):
+        """ get a specific calculation from history"""
+        return History.history.append(calculation)
+    @staticmethod
+    def add_addition_calculation(values):
+        History.add_calculation(Addition(values))
+        return History.get_last_calculation().get_result()
+    @staticmethod
+    def add_subtraction_calculation(values):
+        History.add_calculation(Subtraction(values))
+        return History.get_last_calculation().get_result()
+    @staticmethod
+    def add_multiplication_calculation(values):
+        History.add_calculation(Multiplication(values))
+        return History.get_last_calculation().get_result()
+    @staticmethod
+    def add_division_calculation(values):
+        History.add_calculation(Division(values))
+        return History.get_last_calculation().get_result()
